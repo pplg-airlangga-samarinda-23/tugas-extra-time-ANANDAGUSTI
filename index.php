91c2fa395ae23c7251face6cc1edca3b453ade7d
ItsMeAGS <160562717+ANANDAGUSTI@users.noreply.github.com>
2024-11-26 15:25:48 +0800

    index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
     <div class="login-box">
         <h3>Login</h3>
-        <form action="dashboard.php" method="POST">
+        <form action="login_process.php" method="POST">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Login</button>
